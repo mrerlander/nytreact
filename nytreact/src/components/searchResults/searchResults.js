192a7012e30b6f7ebd234fcd733e5f0634220fd2
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-04-04 01:18:19 -0500

    articles open in new tab

--- nytreact/src/components/searchResults/searchResults.js
+++ nytreact/src/components/searchResults/searchResults.js
@@ -4,7 +4,7 @@ import {Col, Card} from 'react-materialize';
 const SearchResults = props => (
     props.articles.map(article => (
         <Col s={10} offset='s1'>
-            <Card className='blue-grey darken-1 center' textClassName='white-text' title={article.headline.main} actions={[<a href={article.web_url}>Article Link</a>, <a href={article.save}>Save Article</a>]}>
+            <Card className='blue-grey darken-1 center' textClassName='white-text' title={article.headline.main} actions={[<a target='_blank' href={article.web_url}>Article Link</a>, <a href={article.save}>Save Article</a>]}>
             <p><strong>{article.byline.original}</strong> | {article.pub_date} | {article.type_of_material}</p>
             <h6>{article.snippet}</h6>
             </Card>
